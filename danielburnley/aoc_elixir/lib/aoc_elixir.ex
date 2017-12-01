d8bc1fcd2612faeece4f84d493ce68f1c660092b
Daniel Burnley <dan@danops.uk>
2017-12-01 22:00:33 +0000

    solve part 2

--- danielburnley/aoc_elixir/lib/aoc_elixir.ex
+++ danielburnley/aoc_elixir/lib/aoc_elixir.ex
@@ -1,7 +1,8 @@
 defmodule AocElixir do
   def run do
     input = get_input(2017, 1)
-    IO.puts "Day 1.1 - #{AocElixir.DayOne.solve(input)}"
+    IO.puts "Day 1.1 - #{AocElixir.DayOne.PartOne.solve(input)}"
+    IO.puts "Day 1.2 - #{AocElixir.DayOne.PartTwo.solve(input)}"
   end
 
   defp get_input(year, day) do
