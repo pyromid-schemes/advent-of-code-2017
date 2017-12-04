677076682e8b1c5a769aa2726ebfe647725a4694
Daniel Jackson <d95jackson@gmail.com>
2017-12-04 21:29:55 +0000

    part b

--- danj/day04/index.js
+++ danj/day04/index.js
@@ -1,7 +1,6 @@
 const helpers = require('../helpers').default
 
 const day04_a = input => {
-
   return helpers.splitByNewLine(input).reduce((acc, i) => {
     const words = i.split(' ')
     const wordCount = new Set()
@@ -21,7 +20,24 @@ const day04_a = input => {
 }
 
 const day04_b = input => {
-  return 8
+  return helpers.splitByNewLine(input).reduce((acc, i) => {
+    const words = i.split(' ')
+    const wordCount = new Set()
+
+    let valid = true
+    words.some(w => {
+      const wordSorted = w.split('').sort().join('')
+
+      if(wordCount.has(wordSorted)) {
+        valid = false
+        return true
+      }
+      wordCount.add(wordSorted)
+      return false
+    })
+
+    return acc + (valid ? 1 : 0)
+  }, 0)
 }
 
 export default {
--- danj/test/day04.js
+++ danj/test/day04.js
@@ -19,4 +19,22 @@ describe('day 4', () => {
       })
     })
   })
+
+  describe('part B', () => {
+    const tests = [
+      { input: 'abcde fghij', expectedResult: 1 },
+      { input: 'abcde xyz ecdab', expectedResult: 0 },
+      { input: 'a ab abc abd abf abj', expectedResult: 1 },
+      { input: 'iiii oiii ooii oooi oooo', expectedResult: 1 },
+      { input: 'oiii ioii iioi iiio', expectedResult: 0 }
+
+    ]
+
+    tests.map(t => {
+      it(`input '${t.input}' should return ${t.expectedResult}`, () => {
+        const actualResult = day.b(t.input)
+        expect(actualResult).to.equal(t.expectedResult)    
+      })
+    })
+  })
 })
