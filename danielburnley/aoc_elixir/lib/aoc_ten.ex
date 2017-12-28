1758e9f0acdfb078fb20273c66f978fb0b9fe9ab
Daniel Burnley <dan@danops.uk>
2017-12-28 15:46:52 +0000

    Add execute_round function and refactor 10.1

--- danielburnley/aoc_elixir/lib/aoc_ten.ex
+++ danielburnley/aoc_elixir/lib/aoc_ten.ex
@@ -1,4 +1,12 @@
 defmodule AocElixir.DayTen do
+  def execute_round(inputs, marks \\ AocElixir.DayTen.initial_marks(), position \\ 0, skip \\ 0)
+  def execute_round([], marks, position, skip), do: {marks, position, skip}
+  def execute_round([input|rest], marks, position, skip) do
+    marks = AocElixir.DayTen.twist_knot(marks, position, input)
+    new_position = rem(position + input + skip, length(marks))
+    execute_round(rest, marks, new_position, skip + 1)
+  end
+
   def initial_marks, do: (for n <- 0..255, do: n)
 
   def twist_knot(marks, position, amount) when position + amount > length(marks) do
@@ -18,20 +26,19 @@ defmodule AocElixir.DayTen do
       |> execute
     end
 
-    defp execute(inputs, marks \\ AocElixir.DayTen.initial_marks(), position \\ 0, skip \\ 0)
-    defp execute([], marks, _, _) do
-      [first, second|_] = marks
+    defp execute(input) do
+      {[first, second|_], _, _} = AocElixir.DayTen.execute_round(input)
       first * second
-    end
-    defp execute([input|rest], marks, position, skip) do
-      marks = AocElixir.DayTen.twist_knot(marks, position, input)
-      new_position = rem(position + input + skip, 256)
-      execute(rest, marks, new_position, skip + 1)
     end
   end
 
   defmodule PartTwo do
-    def solve(_) do
+    def solve(input) do
+      String.to_charlist(input)
+      |> execute
+    end
+
+    def execute(_) do
     end
   end
 end
--- danielburnley/aoc_elixir/spec/day_ten_spec.exs
+++ danielburnley/aoc_elixir/spec/day_ten_spec.exs
@@ -10,63 +10,73 @@ defmodule DayTenSpec do
     expect(AocElixir.DayTen.PartOne.solve(input)).to eq(expected)
   end
 
-  context "part one" do
-    context "twist knot" do
-      context "acceptance" do
-        it do: expect_marks({[0,1,2,3,4], 0, 3}, [2,1,0,3,4])
-        it do: expect_marks({[2,1,0,3,4], 3, 4}, [4,3,0,1,2])
+  def expect_round({input, marks}, expected) do
+    expect(AocElixir.DayTen.execute_round(input, marks)).to eq(expected)
+  end
+
+  context "twist knot" do
+    context "acceptance" do
+      it do: expect_marks({[0,1,2,3,4], 0, 3}, [2,1,0,3,4])
+      it do: expect_marks({[2,1,0,3,4], 3, 4}, [4,3,0,1,2])
+    end
+
+    context "from position 0" do
+      it "twist nothing" do
+        expect_marks({[1,2,3], 0, 0}, [1,2,3])
       end
 
-      context "from position 0" do
+      it "twist one" do
+        expect_marks({[1,2,3], 0, 1}, [1,2,3])
+      end
+
+      it "twist two" do
+        expect_marks({[1,2,3], 0, 2}, [2,1,3])
+      end
+
+      it "twist whole list" do
+        expect_marks({[1,2,3], 0, 3}, [3,2,1])
+      end
+    end
+
+    context "from non-zero position" do
+      context "not looping" do
         it "twist nothing" do
-          expect_marks({[1,2,3], 0, 0}, [1,2,3])
+          expect_marks({[1,2,3,4,5], 1, 0}, [1,2,3,4,5])
         end
 
         it "twist one" do
-          expect_marks({[1,2,3], 0, 1}, [1,2,3])
+          expect_marks({[1,2,3,4,5], 1, 1}, [1,2,3,4,5])
         end
 
         it "twist two" do
-          expect_marks({[1,2,3], 0, 2}, [2,1,3])
+          expect_marks({[1,2,3,4,5], 1, 2}, [1,3,2,4,5])
         end
 
-        it "twist whole list" do
-          expect_marks({[1,2,3], 0, 3}, [3,2,1])
-        end
-      end
-
-      context "from non-zero position" do
-        context "not looping" do
-          it "twist nothing" do
-            expect_marks({[1,2,3,4,5], 1, 0}, [1,2,3,4,5])
-          end
-
-          it "twist one" do
-            expect_marks({[1,2,3,4,5], 1, 1}, [1,2,3,4,5])
-          end
-
-          it "twist two" do
-            expect_marks({[1,2,3,4,5], 1, 2}, [1,3,2,4,5])
-          end
-
-          it "twist up to end" do
-            expect_marks({[1,2,3,4,5], 1, 4}, [1,5,4,3,2])
-          end
-        end
-        context "looping" do
-          it "twist last and first elements" do
-            expect_marks({[1,2,3,4], 3, 2}, [4,2,3,1])
-          end
+        it "twist up to end" do
+          expect_marks({[1,2,3,4,5], 1, 4}, [1,5,4,3,2])
         end
       end
-    end
-
-    context "solve" do
-      it do: expect_part_one("1", 0)
-      it do: expect_part_one("256", 64770)
-      it do: expect_part_one("3", 2)
-      it do: expect_part_one("3", 2)
-      it do: expect_part_one("1,2", 0)
+      context "looping" do
+        it "twist last and first elements" do
+          expect_marks({[1,2,3,4], 3, 2}, [4,2,3,1])
+        end
+      end
     end
   end
+
+  context "execute_round" do
+    it do: expect_round({[1], [1,2,3,4,5]}, {[1,2,3,4,5], 1, 1})
+    it do: expect_round({[2], [1,2,3,4,5]}, {[2,1,3,4,5], 2, 1})
+    it do: expect_round({[1,2], [1,2,3,4,5]}, {[1,3,2,4,5], 4, 2})
+    it do: expect_round({[1,2,3], [1,2,3,4,5]}, {[1,5,2,4,3], 4, 3})
+  end
+
+  context "solve part one" do
+    it do: expect_part_one("1", 0)
+    it do: expect_part_one("256", 64770)
+    it do: expect_part_one("3", 2)
+    it do: expect_part_one("3", 2)
+    it do: expect_part_one("1,2", 0)
+  end
+
 end
